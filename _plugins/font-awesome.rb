596ae5efe6c50ee82852ef6fb0b5bbda85b6c8b6
Šimon Let <simon.a.let@gmail.com>
2017-11-03 20:24:21 +0100

    new font-awesome.rb

--- _plugins/font-awesome.rb
+++ _plugins/font-awesome.rb
@@ -1,3 +1,36 @@
+##
+# The MIT License (MIT)
+# 
+# Copyright (c) 2014 Ryan Morrissey
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+# 
+# Font Awesome Icons Liquid Tag
+# Documentation can be found at http://fontawesome.io/
+#
+# Example:
+#    {% icon fa-camera-retro %}
+#    {% icon fa-camera-retro fa-lg %}
+#    {% icon fa-spinner fa-spin %}
+#    {% icon fa-shield fa-rotate-90 %}
+
 module Jekyll
   class FontAwesomeTag < Liquid::Tag
 
@@ -8,12 +41,10 @@ module Jekyll
       else
         raise ArgumentError.new <<-eos
 Syntax error in tag 'icon' while parsing the following markup:
-
   #{@markup}
-
 Valid syntax:
-  for icons: <i class="fa fa-camera-retro"></i>
-  for icons with size/spin/rotate: <i class="fa fa-camera-retro fa-lg"></i>
+  for icons: {% icon fa-camera-retro %}
+  for icons with size/spin/rotate: {% icon fa-camera-retro fa-lg %}
 eos
       end
     end
